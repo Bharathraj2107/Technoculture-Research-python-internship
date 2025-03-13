4b8360295e0ee3d426dcc109554268f6ee804490
Bharathraj2107 <bharathraj2105@gmail.com>
2025-03-13 14:59:05 +0530

    third

--- expense-tracker/expenses/models.py
+++ expense-tracker/expenses/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Expense(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)#fields in the database
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
